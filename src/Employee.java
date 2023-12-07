6cfeab939721cb75ebe1e21a18b81ce838a0d648
aya15elsheikh <aya15elsheikh@gmail.com>
2023-12-07 21:33:00 +0200

    Employee class

---
+++ src/Employee.java
@@ -0,0 +1,47 @@
+public class Employee {
+   private int ID;
+   private String password;
+   private String Username;
+   private String Type;
+   public Employee()
+   {
+
+   }
+   public Employee (int ID ,String Username, String password , String Type)
+   {
+       this.ID = ID;
+       this.password = password;
+       this.Username = Username;
+       this.Type = Type;
+   }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setUsername(String username) {
+        Username = username;
+    }
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getType() {
+        return Type;
+    }
+}
