a84feddb3be97db7ae851066bcd0a9b04668cfe8
aya15elsheikh <aya15elsheikh@gmail.com>
2023-12-08 11:41:02 +0200

    files methods

--- src/Inventory_Employee.java
+++ src/Inventory_Employee.java
@@ -1,29 +1,38 @@
 import java.util.Date;
+import java.io.File;
+public class Inventory_Employee extends Employee{
 
-public class Inventory_Employee extends Employee {
-    private
-      String Employee_name ;
+    public Inventory_Employee (int ID ,String Username, String password , String Type)
+    {
+        super( ID , Username, password , Type);
+    }
+
     public boolean Add_product (String name,Date expiry ,String section)
          {   boolean adding_flag =false;
              /*   implementation */
+             create_files c= new create_files();
              return adding_flag;
          }
+
     public boolean Delete_product (int id , String Section)
         {
             boolean del_flag =false;
             /*   implementation */
             return del_flag;
         }
+
     public  boolean update_product(int id, String Section)
         {
             boolean update_flag =false;
             /*   implementation */
             return update_flag;
         }
+
     public String Search_products ( int id , String name)
         {
             return "product name";
         }
+
     public void manage_Damages(int id ,String name)
         {
 
