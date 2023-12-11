4c29901d9bbccaffdb5d2836ed0c0ab3b6f81fad
aya15elsheikh <aya15elsheikh@gmail.com>
2023-12-11 22:57:27 +0200

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!");
+        System.out.print("Hello ! \n");
+//        Inventory_Employee I_E = new Inventory_Employee(11, "Ahmed" ,"1123" , "IN" );
+//        I_E.Add_product("Apple",1011 , "11/12/2023", "FRUIT",10);
+//        I_E.Add_product("Tomato",1011 , "12/10/2024", "vegetable",5);
+//        I_E.Search_products("Tomato");
+//        I_E.Delete_product("Apple");
+//        I_E.Delete_product("Tomato");
+        system s= new system();
+//        s.String_date ("10/11/2022");
+        s.Send_notification();
+        s.Display_notifications();
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+//        System.out.println(s.String_date("2022-11-11"));
+//        create_files c = new create_files();
+//        String s = c.read_line("D:\\FCAI\\y2 sem 1\\pl2\\javacodes\\pl2 project\\products\\Apple.txt", 2);
+//        System.out.println(s);
     }
 }
