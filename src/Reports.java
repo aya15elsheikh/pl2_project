32fda4f35af70050bfdc1f03dfc254d48e01d6af
bassmalahossam24 <bassmalahossam89@gmail.com>
2023-12-12 21:10:15 +0200

    reports

--- src/Reports.java
+++ src/Reports.java
@@ -10,41 +10,10 @@ public class Reports {
     public int get_repoNum(){
         return repo_num;
     }
-    public String description(){
+    public String get_description(){
         return description;
     }
-    public void add_repo(int productId){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Report Number : ");
-        repo_num = input.nextInt();
-        System.out.println("Enter Report description ");
-        description = input.nextLine();
-        System.out.println("Report added Successfully !");
 
-    }
-    public void delete_repo(){
-      repo_num = 0;
-      description = null;
-    }
-    public void edit_repo(){
-        Scanner input = new Scanner(System.in);
-        System.out.println("press 1: for edit report number , press 2: for edit report description ");
-        int choice = input.nextInt();
-        switch (choice){
-            case 1:
-                System.out.println("Enter new report number :  ");
-                repo_num = input.nextInt();
-                break;
-            case 2:
-                System.out.println("Enter new report description :  ");
-                description = input.nextLine();
-                break;
-            default:
-                System.out.println("Invalid choice :(  ");
-
-
-        }
-        System.out.println("Report Edited Successfully :) ");
 
 
     }
@@ -58,4 +27,4 @@ public class Reports {
 
 
 
-}
+
