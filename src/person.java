b209e46c8873bfeec48afcf9e741321a11a71192
shahdemam <131971029+shahdemam@users.noreply.github.com>
2023-12-13 18:40:06 +0200

    Update person.java

--- src/person.java
+++ src/person.java
@@ -1,22 +1,18 @@
 public abstract class person {
    String username;
     String password;
-    int id;
+   
     
 
-    person(String username, String password, int id) {
+    person(String username, String password) {
         this.username = username;
         this.password = password;
-        this.id = id;
+        
     }
 
     public abstract void setusername(String username);
     public abstract String getusername();
 
-    public abstract void setId(String  id);
-    public abstract String  getId();
-    
-  
     public abstract void setPassword(String password);
     public abstract String getPassword();
 }
