4c9874589efd9303fb1b94748dc4f37f522794e1
shahdemam <131971029+shahdemam@users.noreply.github.com>
2023-12-13 20:45:03 +0200

    Update Inventory_Employee.java

--- src/Inventory_Employee.java
+++ src/Inventory_Employee.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 import java.io.FileWriter;
 public class Inventory_Employee extends Employee{
 
-   public Inventory_Employee(String username, String password, String Type, int id) {
-        super(username, password, Type, id);
+    public Inventory_Employee(String username, String password, String Type) {
+        super(username, password, "Inventory_Employee");
     }
 
     public boolean Add_product (String name  , int id , Date expiry ,String section , int quantity,int Special_Range )
