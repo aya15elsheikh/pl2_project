ce8257862b5d530fa5b1a07d7d7a9e273174b161
shahdemam <131971029+shahdemam@users.noreply.github.com>
2023-12-13 12:17:02 +0200

    Update Products.java

--- src/Products.java
+++ src/Products.java
@@ -41,6 +41,10 @@ public class Products {
     public void setExpiry_Date(String Expiry_Date) {
         this.Expiry_Date = Expiry_Date;
     }
+      public void setDamaged_quantity(int Damaged_quantity) {
+        this.Damaged_quantity = Damaged_quantity;
+    }
+
 
     public int getId() {
         return Id;
@@ -64,6 +68,9 @@ public class Products {
 
     public String getExpiry_Date() {
         return Expiry_Date;
+    }
+     public int getDamaged_quantity() {
+        return Damaged_quantity;
     }
 
     
