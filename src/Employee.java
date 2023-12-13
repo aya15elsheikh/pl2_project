b0df7f830250027e8b9adfcaa0bc1aaf1e296837
shahdemam <131971029+shahdemam@users.noreply.github.com>
2023-12-13 15:08:57 +0200

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,34 +1,53 @@
 public class Employee extends person {
-    private String type;
-
-    public Employee(String id, String Username, String password, String type) {
-        super(Username,  password, id);
-        this.type = type;
-    }
     
-     public void setId(String  id) {
+    private String Type;
+
+    public Employee(String Type, String username, String password, int id) {
+        super(username, password, id);
+        this.Type = Type;
+    }
+   @Override
+      public  void setusername(String username){
+          this.username=username;
+      }
+       @Override
+       public String getusername()
+       {
+           return this.username;
+       }
+    
+    
+     
+   @Override
+   public void setId(String  id) {
         this.id =  Integer.parseInt(id);
     }
 
+   @Override
     public String  getId() {
         return String.valueOf(id);
     }
+    
 
+
+   @Override
     public void setPassword(String password) {
         this.password = password;
     }
 
+   @Override
     public String getPassword() {
         return password;
     }
-
-    public void setType(String type) {
-        this.type = type;
+     public void setType(String type) {
+        this.Type = type;
     }
 
     public String getType() {
-        return type;
+        return Type;
     }
+
+     
 
 }
 
