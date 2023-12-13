bd7dceddbdefef027d89a551a7279a112a03c03b
bassmalahossam24 <bassmalahossam89@gmail.com>
2023-12-13 16:46:29 +0200

    Marketing

--- src/Marketing_Employee.java
+++ src/Marketing_Employee.java
@@ -6,14 +6,51 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
 import java.io.FileWriter;
-public class Marketing_Employee extends Employee{
-    public Marketing_Employee (String ID ,String Username, String password , String Type)
-    {
-        super( ID , Username, password , Type);
+public class Marketing_Employee extends Employee {
+    public Marketing_Employee(String ID, String Username, String password, String Type) {
+        super(ID, Username, password, Type);
     }
 
+    public void add_offer(String type, double time, double percentage) {
+        Offers newOffer = new Offers(type, time, percentage);
+        //System.out.println("Offer added Successfully !");
+        try {
+            create_files c = new create_files();
+            String path = "C:\\Users\\MG\\IdeaProjects\\pl2_project\\Offers";
+            c.Create(path, type);
+            c.Append(path +type+".txt", String.valueOf(time));
+            c.Append(path +type+".txt", String.valueOf(percentage));
+            System.out.println("Offer added Successfully !");
+
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+
+        }
+
+    }
+
+    public void add_report(int number, String description, Products product_ifo) {
+        Reports newReport = new Reports(number, description);
+        try {
+            create_files c = new create_files();
+            String path = "C:\\Users\\MG\\IdeaProjects\\pl2_project\\Reports";
+            c.Create(path, String.valueOf(number));
+            c.Append(path +String.valueOf(number)+".txt", description);
+            System.out.println("Report added Successfully !");
+
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+
+        }
 
 
 
-
+    }
 }
+
+
+
+
+
